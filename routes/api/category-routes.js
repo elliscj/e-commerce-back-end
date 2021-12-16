00b144a6979ce0982de222a7b687bb184d2de537
Cam Ellis <elliscj@colorado.edu>
2021-12-15 23:45:50 -0700

    most routes working properly

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -78,7 +78,7 @@ router.delete("/:id", async (req, res) => {
     }
     res.status(200).json(catData);
   } catch (err) {
-    res.status(500).json({ err });
+    res.status(500).json(err);
   }
 });
 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -123,7 +123,7 @@ router.delete("/:id", async (req, res) => {
     }
     res.status(200).json(prodData);
   } catch (err) {
-    res.status(500).json({ err });
+    res.status(500).json(err);
   }
 });
 
